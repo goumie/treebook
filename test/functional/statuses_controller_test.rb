8f6dbafbeb8070dc6bdb33bb49f5b3ab7f36582b
Christos <goumenos.c@gmail.com>
2013-03-10 23:48:11 -0500

    Add status validations and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -24,7 +24,14 @@ class StatusesControllerTest < ActionController::TestCase
 
   end
 
-  test "should create status" do
+  test "need to be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:christos)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -38,11 +45,14 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in users(:christos)
     get :edit, id: @status
     assert_response :success
   end
 
   test "should update status" do
+    
+    sign_in users(:christos)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires conent" do 
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+test "status is at least two letters" do
+	status = Status.new
+	status.content = "H"
+	assert !status.save
+	assert !status.errors[:content].empty?
+
+end
+
+test "status has to have a user id" do
+	status= Status.new
+	status.content = "Hello"
+	assert !status.save
+	assert !status.errors[:user_id].empty?
+end
+
 end
